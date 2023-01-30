8a8d2ac42e966627a5b393a019495b4b8fb864cf
Laurens Jan van Haaren <l.van.haaren@fz-juelich.de>
2023-01-30 17:37:45 +0100

    Versuch Test FIX 2

--- datalad_cds_extension/tests/test_register.py
+++ datalad_cds_extension/tests/test_register.py
@@ -5,7 +5,7 @@ def test_register():
     import datalad.api as da
     assert hasattr(da, 'datalad_cds_extension')
     assert_result_count(
-        da.datalad_cds(),
+        da.datalad_cds_extension(),
         1,
         action='demo')
 
