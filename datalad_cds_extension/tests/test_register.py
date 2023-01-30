5471a290f45a954702bcf81c60235fee182ddf0e
Laurens Jan van Haaren <l.van.haaren@fz-juelich.de>
2023-01-30 17:35:43 +0100

    Versuch FIX test

--- datalad_cds_extension/tests/test_register.py
+++ datalad_cds_extension/tests/test_register.py
@@ -3,7 +3,7 @@ from datalad.tests.utils_pytest import assert_result_count
 
 def test_register():
     import datalad.api as da
-    assert hasattr(da, 'datalad_cds')
+    assert hasattr(da, 'datalad_cds_extension')
     assert_result_count(
         da.datalad_cds(),
         1,
